2e673d6a71591bd4e8a2f891079ee41107a1ada7
kolisachint <kolisachint@gmail.com>
2018-11-25 05:12:28 -0500

    Day2

--- src/medium-to-markdown_mod.js
+++ src/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/swlh/what-i-learned-about-procrastination-while-scaling-my-startup-to-4-2-million-users-b07ba29309e").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@priyansh.jain0246/how-i-automated-the-boring-university-stuff-with-python-f2935b10d2ce").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
